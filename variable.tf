c06d148252925a314e031077d3cca521b1eb9ccf
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-01-18 12:51:44 +0200

    update

--- variable.tf
+++ variable.tf
@@ -1,19 +1,15 @@
 variable "project" {
   type = string
 }
-
 variable "region" {
   type = string
 }
-
 variable database {
   type = map
 }
-
 variable servers {
   type = map
 }
-
 variable agents {
   type = map
 }
